3113504e8a3f3a85b988b3f62b94a3a7d9dfae8c
Justin Anderson <justinanderson101@gmail.com>
2021-04-14 16:03:41 -0600

    implemented Form component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,8 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -160,10 +162,7 @@ storiesOf("Appointment", module)
   .add("Status-Saving", () => <Status message="Saving.." />)
   .add("Status-Deleting", () => <Status message="Deleting.." />)
   .add("Error-Saving", () => (
-    <Error
-      message="Could not save appointment."
-      onClose={action("onClose")}
-    />
+    <Error message="Could not save appointment." onClose={action("onClose")} />
   ))
   .add("Error-Deleting", () => (
     <Error
@@ -171,3 +170,19 @@ storiesOf("Appointment", module)
       onClose={action("onClose")}
     />
   ))
+  .add("Form-Edit", () => (
+    <Form
+      name="Justin Anderson"
+      interviewers={interviewers}
+      interviewer={interviewers.id}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form-Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ));
