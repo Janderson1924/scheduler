b4d794fa893dfaf802a81515b040d26e02a307bc
Justin Anderson <justinanderson101@gmail.com>
2021-04-15 17:03:29 -0600

    added Axios, made axios request for days array, removed hardcoded 'days' array

--- stories/index.js
+++ stories/index.js
@@ -47,23 +47,6 @@ storiesOf("DayListItem", module)
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
   ));
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 storiesOf("DayList", module)
   .addParameters({
