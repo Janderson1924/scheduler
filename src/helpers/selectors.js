c4d6a1685c91c992a3984690746701cf47726b29
Justin Anderson <justinanderson101@gmail.com>
2021-04-15 17:37:54 -0600

    implemented getAppointmentsForDay function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,9 @@
+export function getAppointmentsForDay(state, day) {
+  const selectedDay = state.days.filter(dayFilter => dayFilter.name === day);
+
+  if (state.days.length === 0 || selectedDay.length === 0) {
+    return [];
+  }
+  const appointments = selectedDay[0].appointments.map((appointment) => state.appointments[appointment]);
+    return appointments;
+}
