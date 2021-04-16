27df8359266a40cc7da05dd1cfbf8f8592a60cdd
Justin Anderson <justinanderson101@gmail.com>
2021-04-16 16:18:50 -0600

    finished building useVisualMode custom hook, passing all tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 
 export default function useVisualMode(initial) {
+  
   const [mode, setMode] = useState(initial);
-  function transition(newMode) {
+  const [history, setHistory] = useState([initial]);
+  
+  function transition(newMode, replace = false) {
+    if (replace) {
+    setHistory(prev => {
+      let newHistory = prev.slice(0, -1);
+      return [...newHistory, newMode]
+    })
+  } else {
+    setHistory(prev => [...prev, newMode])
+    }
     setMode(newMode);
   }
+
+
+  function back () {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1])
+    }
+  }
   
-  return { mode, transition };
+  return { mode, transition, back };
 }
