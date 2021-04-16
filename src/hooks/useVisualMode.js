daa9fec82db6091d699bd2b01b5fd747693db7c8
Justin Anderson <justinanderson101@gmail.com>
2021-04-16 15:31:17 -0600

    implemented transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  function transition(newMode) {
+    setMode(newMode);
+  }
   
-  return { mode };
+  return { mode, transition };
 }
