51ae0e805e4188f084e42a024493ff283ca59731
Justin Anderson <justinanderson101@gmail.com>
2021-04-18 16:38:46 -0600

    implemented spotsRemaining and updatedDays functions to show spots remaining for each day

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -11,6 +11,24 @@ export default function useApplicationData() {
 
   const setDay = (day) => setState({ ...state, day });
 
+  const spotsRemaining = (dayOfWeek, days, appointments) => {
+    let spotsLeft = 0;
+    const chosenDay = days.find(day => day.name === dayOfWeek);
+    for (let appointment in appointments) {
+      if (appointments[appointment].interview === null && chosenDay.appointments.includes(appointments[appointment].id)) {
+        spotsLeft++
+      }
+    }
+    return { 
+      ...chosenDay, 
+      spots: spotsLeft 
+    };
+  };
+
+  const updatedDays = (dayObject, arrayOfDays) => {
+    return arrayOfDays.map((day) => (day.name === dayObject.name ? dayObject : day));
+  };
+
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -20,11 +38,11 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
-    setState({
-      ...state,
-      appointments,
-    });
-    return axios.put(`api/appointments/${id}`, { interview });
+    const days = updatedDays(spotsRemaining(state.day, state.days, appointments), state.days);
+    return axios.put(`api/appointments/${id}`, { interview })
+    .then(() => setState((prev) => {
+      return { ...prev, appointments, days };
+    }));
   }
 
   function cancelInterview(id) {
@@ -36,11 +54,11 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
-    setState({
-      ...state,
-      appointments,
-    });
-    return axios.delete(`/api/appointments/${id}`, { interview: null });
+    const days = updatedDays(spotsRemaining(state.day, state.days, appointments), state.days);
+    return axios.delete(`/api/appointments/${id}`, { interview: null })
+    .then(() => setState((prev) => {
+      return { ...prev, appointments, days };
+    }));
   }
 
   useEffect(() => {
