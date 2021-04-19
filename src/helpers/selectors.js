a56b421e2b22c26429c55375eb5ce872a94cdfc3
Justin Anderson <justinanderson101@gmail.com>
2021-04-19 16:12:37 -0600

    refactored getInterviewersForDay and its tests to remove error

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -10,7 +10,7 @@ export function getAppointmentsForDay(state, chosenDay) {
 export function getInterviewersForDay(state, chosenDay) {
   for (let day of state.days) {
     if (day.name === chosenDay) {
-      return day.interviewers.map(id => state.interviewers[id]);
+      return day.interviewers.map(interviewer => state.interviewers[interviewer]);
     }
   }
   return [];
@@ -23,11 +23,11 @@ export function getInterview(state, interview) {
   let interviewerScheduled = {}
   for (let key in state.interviewers) {
     if (state.interviewers[key].id === interview.interviewer)
-      interviewerScheduled = state.interviewers[key]
+    interviewerScheduled = state.interviewers[key]
   }
   const interviewObj = {
     student: interview.student,
     interviewer: interviewerScheduled
   };
-  return interviewObj
-}
+  return interviewObj;
+};
